8bfb90ab7d7b0371ae231ad01811b0269ef52f53
CristinaMora <Crmora03@ucm.es>
2023-04-21 13:20:54 +0200

    asdfgh

--- Asteroids3/TPV2/TPV2/src/states/MainMenuState.cpp
+++ Asteroids3/TPV2/TPV2/src/states/MainMenuState.cpp
@@ -81,32 +81,15 @@ void MainMenuState::server(const int port) {
 	SDLNet_SocketSet socketSet = SDLNet_AllocSocketSet(1);
 	SDLNet_UDP_AddSocket(socketSet, sd);
 
-	// MODIFICAR ESTE CODIGO PARA QUE SEA NUESTRO JUEGO
-	
-	bool end = false;
-	cout << "ENTRA" << endl;
-	while (!end) {
-		// TODO I: PROCESS DATA on socket sd
-		//actividad en clientes
-		if (SDLNet_CheckSockets(socketSet, SDL_MAX_UINT32) > 0) {
-			if (SDLNet_SocketReady(sd)) {
-				if (SDLNet_UDP_Recv(sd, p) > 0) {
-					// print client’s message
-					cout << "Client says: " << buffer << endl;
-					// send a response
-					memcpy(buffer, "Received!", 10);
-					p->len = 10;
-					SDLNet_UDP_Send(sd, -1, p);
-				}
-			}
-		}
-	}
+	GameStateMachine::instance()->popState();
+	GameStateMachine::instance()->pushState(new PlayStateMultiPlayer());
+	GameStateMachine::instance()->pushState(new PauseState());
 
 	// ESTO DE AQUI DEBE LLAMARSE EN LA DESTRUCTORA DEL JUEGO
 	// libera 
-	SDLNet_FreePacket(p);
+	/*SDLNet_FreePacket(p);
 	SDLNet_FreeSocketSet(socketSet);
-	SDLNet_UDP_Close(sd);
+	SDLNet_UDP_Close(sd);*/
 }
 
 // Conecta con el host y el puerto
@@ -122,28 +105,14 @@ void MainMenuState::client(const char* host, const int port) {
 	SDLNet_SocketSet socketSet = SDLNet_AllocSocketSet(1);
 	SDLNet_UDP_AddSocket(socketSet, sd);
 
-	// MODIFICAR ESTE CODIGO PARA QUE SEA NUESTRO JUEGO
-	while (1) {
-		// TODO I: PROCESS DATA on socket sd
-		cout << "Enter a message: ";
-		cin.getline(buffer, 255);
-		if (strcmp(buffer, "exit") == 0) { break; }
-		p->len = static_cast<int>(strlen(buffer)) + 1;
-		p->address = srvadd;
-		SDLNet_UDP_Send(sd, -1, p);
-		if (SDLNet_CheckSockets(socketSet, 3000) > 0) {
-			if (SDLNet_SocketReady(sd)) {
-				while (SDLNet_UDP_Recv(sd, p) > 0) {
-					cout << "Server says: " << buffer << endl;
-				}
-			}
-		}
-	}
-
+	GameStateMachine::instance()->popState();
+	GameStateMachine::instance()->pushState(new PlayStateMultiPlayer());
+	GameStateMachine::instance()->pushState(new PauseState());
+	
 	// ESTO DE AQUI DEBE LLAMARSE EN LA DESTRUCTORA DEL JUEGO
-	SDLNet_FreePacket(p);
-	SDLNet_FreeSocketSet(socketSet);
-	SDLNet_UDP_Close(sd);
+	//SDLNet_FreePacket(p);
+	//SDLNet_FreeSocketSet(socketSet);
+	//SDLNet_UDP_Close(sd);
 }
 
 // UI
